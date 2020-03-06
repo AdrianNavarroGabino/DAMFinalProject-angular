fb583295a601f7481ec7915ef3b9e8b2f2cb1c0f
Adrián Navarro Gabino <adriannavarrogabino@gmail.com>
2020-03-06 19:13:34 +0100

    Components

--- libros-app/src/app/app.module.ts
+++ libros-app/src/app/app.module.ts
@@ -6,10 +6,18 @@ import { HeaderComponent } from './header/header.component';
 import { RouterModule, Routes } from '@angular/router';
 import { InicioComponent } from './inicio/inicio.component';
 import { FooterComponent } from './footer/footer.component';
+import { ExplorarComponent } from './explorar/explorar.component';
+import { LibrosComponent } from './libros/libros.component';
+import { NotificacionesComponent } from './notificaciones/notificaciones.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/inicio', pathMatch: 'full'},
-  {path: 'inicio', component: InicioComponent}
+  {path: 'inicio', component: InicioComponent},
+  {path: 'explorar', component: ExplorarComponent},
+  {path: 'libros', component: LibrosComponent},
+  {path: 'notificaciones', component: NotificacionesComponent},
+  {path: 'perfil', component: PerfilComponent}
 ]
 
 @NgModule({
@@ -17,7 +25,11 @@ const routes: Routes = [
     AppComponent,
     HeaderComponent,
     InicioComponent,
-    FooterComponent
+    FooterComponent,
+    ExplorarComponent,
+    LibrosComponent,
+    NotificacionesComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
